c8f45e2a3dcb8a015a81076eb7896d14db7c6ee9
glanisacat <borovskhih@gmail.com>
2023-03-10 16:42:25 +0200

    calc finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'mycalculator';
+  toShow = '0';
+  currentValue = '';
+
+  writeToInput(value:string) {
+    this.currentValue =  this.currentValue + value
+    this.toShow = this.currentValue
+  };
+
+  equals(){ 
+    this.toShow = eval(this.currentValue)
+    this.currentValue = this.toShow
+  }
+
+  clear(){
+    this.currentValue = ''
+    this.toShow = this.currentValue
+  }
+
+  back(){
+    this.currentValue = this.currentValue.slice(0, -1)
+    this.toShow = this.currentValue
+    if(this.toShow == ''){
+      this.toShow = '0'
+    }
+  }
+
+  calcValue(solve:any){
+    if(solve.charAt(0) == '0'){
+      solve = solve.slice(1,)
+    }
+    this.currentValue = this.toShow
+    this.toShow = eval(solve)
+  }
 }
