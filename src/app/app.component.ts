4ff44991b5277ba4e69f1ddea75a1cde51482895
glanisacat <borovskhih@gmail.com>
2023-03-10 19:01:28 +0200

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,41 +3,41 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'mycalculator';
   toShow = '0';
   currentValue = '';
 
-  writeToInput(value:string) {
-    this.currentValue =  this.currentValue + value
-    this.toShow = this.currentValue
-  };
-
-  equals(){ 
-    this.toShow = eval(this.currentValue)
-    this.currentValue = this.toShow
+  writeToInput(value: string) {
+    this.currentValue = this.currentValue + value;
+    this.toShow = this.currentValue;
   }
 
-  clear(){
-    this.currentValue = ''
-    this.toShow = this.currentValue
+  equals() {
+    this.toShow = eval(this.currentValue);
+    this.currentValue = this.toShow;
   }
 
-  back(){
-    this.currentValue = this.currentValue.slice(0, -1)
-    this.toShow = this.currentValue
-    if(this.toShow == ''){
-      this.toShow = '0'
+  clear() {
+    this.currentValue = '';
+    this.toShow = this.currentValue;
+  }
+
+  back() {
+    this.currentValue = this.currentValue.slice(0, -1);
+    this.toShow = this.currentValue;
+    if (this.toShow == '') {
+      this.toShow = '0';
     }
   }
 
-  calcValue(solve:any){
-    if(solve.charAt(0) == '0'){
-      solve = solve.slice(1,)
+  calcValue(solve: any) {
+    if (solve.charAt(0) == '0') {
+      solve = solve.slice(1);
     }
-    this.currentValue = this.toShow
-    this.toShow = eval(solve)
+    this.currentValue = this.toShow;
+    this.toShow = eval(solve);
   }
 }
